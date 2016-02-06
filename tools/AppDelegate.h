506d034fe5732dab6cf022f1e16e9abc0d6f522e
anyixiaoyue <460303556@qq.com>
2016-02-06 23:28:55 +0800

    Initial Commit

---
+++ tools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tools
+//
+//  Created by Shawn on 2/6/16.
+//  Copyright © 2016 Shawn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tools
+//
+//  Created by Shawn on 2/6/16.
+//  Copyright © 2016 Shawn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
